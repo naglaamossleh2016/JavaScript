0226151332c44dd119d9bc5397d614503af08982
Naglaa <naglaa.mossleh@gmail.com>
2023-02-02 12:11:46 +0200

    change variable

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,6 +2,6 @@ const defaultValue = 0;
 let currentResult = defaultValue;
 currentResult = currentResult + (10 * 3) / 2 - 1;
 
-let resultDescription = "( " + defaultValue + " +  10 * 3 ) / 2-1 ";
+let resultDescription = "(" + defaultValue + " + 10 * 3 ) / 2-1";
 
 outputResult(currentResult, resultDescription);
