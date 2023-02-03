6bee7332bd0f80dcd858d864456d3756fc5adbd9
naglaamossleh2016 <naglaa>
2023-02-03 23:26:09 +0200

    assignment2

--- basics-assignment-2-start/assignment.js
+++ basics-assignment-2-start/assignment.js
@@ -1,1 +1,17 @@
 const task3Element = document.getElementById('task-3');
+
+function Hello(){
+    alert('Hello World!');
+}
+function HelloUser(name){
+    alert('Hello '+name);
+}
+Hello();
+HelloUser('Naglaa');
+task3Element.addEventListener('click',Hello);
+
+function fullName(firstName,middleName,lastName){
+    return firstName+' '+middleName+' '+lastName;
+}
+let name=fullName('Naglaa','Abu Bakr',"Mossleh");
+alert(name);
