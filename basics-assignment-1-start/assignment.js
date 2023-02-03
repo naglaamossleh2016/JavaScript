767a30789f68bb78e3586a324efba22d7b2fdb44
naglaamossleh2016 <naglaa>
2023-02-03 21:11:09 +0200

    solving assignment1

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,9 @@
+const userInput=10;
+let result;
+result=18;
+result=result+userInput;
+result=result-10;
+result=result*10;
+result=result/10;
+alert(userInput);
+alert(result);
---
+++ basics-assignment-1-start/index.js
@@ -0,0 +1,10 @@
+function subtract(num1,num2){
+    let result=num1-num2;
+    //alert(result);
+    return alert(result);
+}
+function stringify(number){
+    return alert("result: "+number);
+}
+subtract(12,10);
+stringify(10);
