fb89c582d8faace1b36844017d2341c9b025b62b
Naglaa <naglaa.mossleh@gmail.com>
2023-02-04 12:26:58 +0200

    Adding a Re-Usable Function That Uses Objects

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -11,36 +11,43 @@ function displayValue(numberBerforeCal, operator, resultAfterCal) {
   outputResult(currentResult, DescInput);
 }
 
+function showLog(operation, intialValue, UserNum, summtion) {
+  const logObject = {
+    operation: operation,
+    initialVal: intialValue,
+    inputUser: UserNum,
+    sum: summtion,
+  };
+  logEnteries.push(logObject);
+  console.log(logEnteries);
+}
 function add() {
   const UserNum = getUserNumberInput();
   const intialValue = currentResult;
   currentResult += UserNum;
   displayValue(intialValue, "+", UserNum);
-  const logObject = {
-    operation: "ADD",
-    initialVal: intialValue,
-    inputUser: UserNum,
-    sum: currentResult,
-  };
-  console.log(logObject);
+  showLog("+", intialValue, UserNum, currentResult);
 }
 function subtract() {
   const UserNum = getUserNumberInput();
   const intialValue = currentResult;
   currentResult -= UserNum;
   displayValue(intialValue, "-", UserNum);
+  showLog("-", intialValue, UserNum, currentResult);
 }
 function multiply() {
   const UserNum = getUserNumberInput();
   const intialValue = currentResult;
   currentResult *= UserNum;
   displayValue(intialValue, "*", UserNum);
+  showLog("*", intialValue, UserNum, currentResult);
 }
 function devision() {
   const UserNum = getUserNumberInput();
   const intialValue = currentResult;
   currentResult /= UserNum;
   displayValue(intialValue, "/", UserNum);
+  showLog("/", intialValue, UserNum, currentResult);
 }
 addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
