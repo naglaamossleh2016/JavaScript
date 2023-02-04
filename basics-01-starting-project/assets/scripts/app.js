101dc66e6fc2ec041eb2b8b76f6b1fa355779926
Naglaa <naglaa.mossleh@gmail.com>
2023-02-04 10:13:25 +0200

    Connecting all Buttons to Functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,40 @@
 const defaultValue = 0;
 let currentResult = defaultValue;
+
 function getUserNumberInput() {
   return parseInt(userInput.value);
 }
-function add() {
-  const UserNum = getUserNumberInput();
-  const DescInput = `${currentResult} + ${UserNum}`;
-  currentResult = currentResult + UserNum;
+
+function displayValue(numberBerforeCal, operator, resultAfterCal) {
+  const DescInput = `${numberBerforeCal} ${operator} ${resultAfterCal}`;
   outputResult(currentResult, DescInput);
 }
+
+function add() {
+  const UserNum = getUserNumberInput();
+  const intialValue = currentResult;
+  currentResult = currentResult + UserNum;
+  displayValue(intialValue, "+", UserNum);
+}
+function subtract() {
+  const UserNum = getUserNumberInput();
+  const intialValue = currentResult;
+  currentResult = currentResult - UserNum;
+  displayValue(intialValue, "-", UserNum);
+}
+function multiply() {
+  const UserNum = getUserNumberInput();
+  const intialValue = currentResult;
+  currentResult = currentResult * UserNum;
+  displayValue(intialValue, "*", UserNum);
+}
+function devision() {
+  const UserNum = getUserNumberInput();
+  const intialValue = currentResult;
+  currentResult = currentResult / UserNum;
+  displayValue(intialValue, "/", UserNum);
+}
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", devision);
