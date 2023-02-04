b09cd4e2072ef1fee3deba56aa48950c061390b0
Naglaa <naglaa.mossleh@gmail.com>
2023-02-04 09:26:52 +0200

    convert string to int

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -5,3 +5,4 @@ currentResult = currentResult + (10 * 3) / 2 - 1;
 let resultDescription = "(" + defaultValue + " + 10 * 3 ) / 2-1";
 
 outputResult(currentResult, resultDescription);
+////////////////////////////////////////////
--- basics-01-starting-project/assets/scripts/vendor.js
+++ basics-01-starting-project/assets/scripts/vendor.js
@@ -1,13 +1,21 @@
-const userInput = document.getElementById('input-number');
-const addBtn = document.getElementById('btn-add');
-const subtractBtn = document.getElementById('btn-subtract');
-const multiplyBtn = document.getElementById('btn-multiply');
-const divideBtn = document.getElementById('btn-divide');
+const userInput = document.getElementById("input-number");
+const addBtn = document.getElementById("btn-add");
+const subtractBtn = document.getElementById("btn-subtract");
+const multiplyBtn = document.getElementById("btn-multiply");
+const divideBtn = document.getElementById("btn-divide");
 
-const currentResultOutput = document.getElementById('current-result');
-const currentCalculationOutput = document.getElementById('current-calculation');
+const currentResultOutput = document.getElementById("current-result");
+const currentCalculationOutput = document.getElementById("current-calculation");
 
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
 }
+const defaultValue = 0;
+let currentResult = defaultValue;
+
+function add() {
+  currentResult = currentResult + parseInt(userInput.value);
+  outputResult(currentResult, "");
+}
+addBtn.addEventListener("click", add);
