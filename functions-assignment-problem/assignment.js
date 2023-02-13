8556dafd84c8d986b3c8d45643983e2b08644a52
Naglaa <naglaa.mossleh@gmail.com>
2023-02-13 11:29:13 +0200

    function 1&2&3

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,22 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
-}
+const sayHello = (userName) => {
+  console.log("Hi " + userName);
+};
+sayHello("Naglaa");
+const DEFAULT_VALUE = "HI";
+const greetUser1 = (userName, phrase = DEFAULT_VALUE) => {
+  console.log(phrase + " " + userName);
+};
+greetUser1("Naglaa", "Hello");
+greetUser1("noga");
+const greetUser2 = () => {
+  console.log("Hi Baby");
+};
+greetUser2();
 
-sayHello();
+const greetUser3 = (userName) => "hi " + userName;
+
+console.log(greetUser3("ghghgh"));
+/////////////////////////////
+const value = function checkInput(...value) {
+  console.log(value);
+};
