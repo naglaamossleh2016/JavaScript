229eb57875704f34dba3b9db1ccfb1ba6c7b1dc3
Naglaa <naglaa.mossleh@gmail.com>
2023-02-16 12:02:28 +0200

    solved assignment

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,16 @@
+const thisTask1 = document.querySelector("#task-1");
+thisTask1.style.color = "White";
+thisTask1.style.backgroundColor = "black";
+const thisTask2 = document.getElementById("task-1");
+thisTask2.style.color = "White";
+thisTask2.style.backgroundColor = "black";
+
+//const headTitle1 = document.head;
+const titleEl = document.head.querySelector("title");
+//titleEl.textContent = "Assignment - Solved!";
+
+const headTitle2 = document.querySelector("title");
+headTitle2.textContent = "Assignment - Solved!";
+
+const h1 = document.querySelector("h1");
+h1.textContent = "Assignment - Solved!";
