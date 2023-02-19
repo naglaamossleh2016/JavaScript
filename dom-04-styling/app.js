cd639d2abca95203cd29186379383b3a16bab79a
Naglaa <naglaa.mossleh@gmail.com>
2023-02-19 10:34:01 +0200

    coding exercise

--- dom-04-styling/app.js
+++ dom-04-styling/app.js
@@ -3,3 +3,7 @@ const section = document.querySelector("section");
 button.addEventListener("click", () => {
   section.classList.toggle("invisible");
 });
+const h2 = document.createElement("h2");
+section.appendChild(h2);
+h2.textContent = "H2 Heading";
+h2.style.backgroundColor = "black";
