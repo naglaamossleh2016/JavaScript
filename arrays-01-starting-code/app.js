8eebec4afa7aed2db147fb5eef54e03254928a18
Naglaa <naglaa.mossleh@gmail.com>
2023-02-22 10:29:17 +0200

    array

---
+++ arrays-01-starting-code/app.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+const newArray = new Array(3, 2, 3);
+console.log(newArray);
+const arrayOf = Array.of(1, 2, 3);
+console.log(arrayOf);
+const arrayFrom = Array.from([1, 2, 3]);
+console.log(arrayFrom);
--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -2,17 +2,69 @@ const model = document.querySelector(".modal");
 const addBtn = document.querySelector("header button");
 const backDrop = document.getElementById("backdrop");
 const cancelBtn = document.querySelector(".btn--passive");
-
+const inputUsers = document.querySelectorAll("input");
+const addMovieDataBtn = document.querySelector(".btn--success");
+const sectionText = document.querySelector("#entry-text");
+const movies = [];
+/////////////////
+function updateUI() {
+  if (movies.length === 0) {
+    sectionText.style.display = "block";
+  } else {
+    sectionText.style.display = "none";
+  }
+}
+function renderNewMovieEl(title, imageUrl, rate) {
+  const newMovieEl = document.createElement("li");
+  newMovieEl.className = "movie-element";
+  newMovieEl.innerHTML = `<div class="movie-element__info"><img src="${imageUrl}" alt="${title}"/></div>
+  <div  class="movie-element__info"><h2>${title}</h2><p>${rate}/5 Stars</p></div>
+  `;
+  const listRoot = document.getElementById("movie-list");
+  listRoot.append(newMovieEl);
+}
 function toggleModel() {
   model.classList.toggle("visible");
   backdropHandler();
 }
 function backdropHandler() {
-  back.classList.toggle("visible");
+  backDrop.classList.toggle("visible");
+}
+function clearMovieData() {
+  for (const inputuser of inputUsers) inputuser.value = "";
 }
 function cancelBtnHandler() {
   toggleModel();
+  clearMovieData();
+}
+
+function addMovieData() {
+  const titleUser = inputUsers[0].value;
+  const imageUrl = inputUsers[1].value;
+  const ratingValue = inputUsers[2].value;
+  if (
+    titleUser.trim() === "" ||
+    imageUrl.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Enter a valid value (Rating value should be between 1 and 5)");
+    return;
+  }
+  const moviesData = {
+    title: titleUser,
+    image: imageUrl,
+    rating: ratingValue,
+  };
+  movies.push(moviesData);
+  console.log(movies);
+  updateUI();
+  renderNewMovieEl(titleUser, imageUrl, ratingValue);
+  toggleModel();
+  clearMovieData();
 }
 addBtn.addEventListener("click", toggleModel);
 cancelBtn.addEventListener("click", cancelBtnHandler);
 backDrop.addEventListener("click", cancelBtnHandler);
+addMovieDataBtn.addEventListener("click", addMovieData);
