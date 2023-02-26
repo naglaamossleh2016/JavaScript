d76fac4f694496844e6577c72b894687ae544fc1
Naglaa <naglaa.mossleh@gmail.com>
2023-02-26 12:39:25 +0200

    object

---
+++ oop/oop.js
@@ -0,0 +1,11 @@
+let user = {
+  firstName: "Naglaa",
+  lastName: "Mossleh",
+  greet: function () {
+    return `${user.firstName}`;
+  },
+};
+
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.greet);
