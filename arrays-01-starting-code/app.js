6ee63f4205b3b9e49524da926f63f4d1e2b99cd8
Naglaa <naglaa.mossleh@gmail.com>
2023-02-26 11:59:14 +0200

    array

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -1,8 +1,13 @@
 const numbers = [1, 2, 3];
-console.log(numbers);
-const newArray = new Array(3, 2, 3);
-console.log(newArray);
-const arrayOf = Array.of(1, 2, 3);
-console.log(arrayOf);
-const arrayFrom = Array.from([1, 2, 3]);
-console.log(arrayFrom);
+const arrayEl = ["Naglaa", "Abo Bakr"];
+arrayEl.push("Mossleh"); // insert element in the last postion of array
+arrayEl.unshift("Eng"); //insert element in the first postion
+console.log(arrayEl.pop()); //remove last element
+console.log(arrayEl.shift()); //remove first element
+console.log(arrayEl);
+arrayEl[1] = "Fawi";
+console.log(arrayEl); //replace element in specifed postion
+arrayEl.splice(1, 0, "Om Hussien"); //insert element in specified index
+console.log(arrayEl);
+arrayEl.splice(0, 3);
+console.log(arrayEl);
