4d79ead0ab3306a45963a92c5dc8f1bb476bff9a
Naglaa <naglaa.mossleh@gmail.com>
2023-02-28 09:37:15 +0200

    object

--- oop/oop.js
+++ oop/oop.js
@@ -1,11 +1,27 @@
 let user = {
   firstName: "Naglaa",
   lastName: "Mossleh",
-  greet: function () {
-    return `${user.firstName}`;
+  age: 37,
+  address: {
+    eg: "Egypt",
+    usa: "california",
+    ksa: "Riyadh",
+    getAddress: () => `Main Address ${user.address.eg}`,
   },
+  greet: function () {
+    return `${user.firstName} ${user.lastName}`;
+  },
+  ageInDays: () => `get user age ${user.age}`,
 };
+
+let newObj={
+  1:"One",
+  2:"Two"
+}
 
 console.log(user.firstName);
 console.log(user.lastName);
-console.log(user.greet);
+console.log(user.greet());
+console.log(user.address.getAddress());
+console.log(user["address"]["ksa"]);
+console.log(user.age);
